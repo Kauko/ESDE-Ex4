644a10cb029c97f444728497c2dabf511864aa3e
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2013-04-16 11:25:06 +0300

    Fixed first part of exercise to follow better paradigm

--- DiceDistribution/src/fi/oulu/tol/dicedistribution/DiceDistributionActivity.java
+++ DiceDistribution/src/fi/oulu/tol/dicedistribution/DiceDistributionActivity.java
@@ -11,19 +11,54 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class DiceDistributionActivity extends Activity {
-    public static final int ROUNDS = 1000;
+    public static final int ROUNDS = 100;
     public static final int ROUND_DELAY = 50; // milliseconds
 	
     private TextView roundView;
     private ProgressBar[] bars;
-    private int counts[];
+    private int counts[] = new int[11];
     private int maximumCount = 0;
+    
+    int index;
+	final Random random = new Random();
+	
+    Thread thread = new Thread () {
+    	public void run() {
+			try {
+				for (index = 0; index < ROUNDS-1; index++) {
+					int die1 = random.nextInt(6);
+					int die2 = random.nextInt(6);
+					int sum = die1 + die2;
+					counts[sum]++;
+					
+					if (counts[sum] > maximumCount)
+						maximumCount = counts[sum];
+					
+					roundView.post(runnable);
+					Thread.sleep(ROUND_DELAY);
+				}
+			} catch (InterruptedException e) {
+					
+			}
+		}
+    };
+    
+    Runnable runnable = new Runnable () {
+    	public void run() {
+			roundView.setText(Integer.toString(index + 1));
+			
+			for (int k = 0; k < 11; k++) {
+				bars[k].setMax(maximumCount);
+				bars[k].setProgress(counts[k]);
+			}	
+		}
+    };
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         createUI();
-        castDice();
+        thread.start();
 	}
 	
     
