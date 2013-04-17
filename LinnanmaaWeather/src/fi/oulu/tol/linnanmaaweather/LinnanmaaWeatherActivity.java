613c226ff02f8711051864c9c6ef3d05250718ce
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2013-04-17 10:52:41 +0300

    Second part kind of works. There's still some work that could be put into it, but I'd rather focus on something else.

--- LinnanmaaWeather/src/fi/oulu/tol/linnanmaaweather/LinnanmaaWeatherActivity.java
+++ LinnanmaaWeather/src/fi/oulu/tol/linnanmaaweather/LinnanmaaWeatherActivity.java
@@ -1,40 +1,28 @@
 package fi.oulu.tol.linnanmaaweather;
 
-import java.io.IOException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.util.EntityUtils;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.net.http.AndroidHttpClient;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import fi.oulu.tol.linnanmaaweather.LinnanmaaWeatherService.LinnanmaaWeatherBinder;
 
 public class LinnanmaaWeatherActivity extends Activity {
-	private static final String WEATHER_URI = "http://weather.willab.fi/weather.xml";
+	
 	
 	private TextView mTemperatureLabel;
-	private Button mRefreshButton;
 	
 	private LinnanmaaWeatherService mWeatherService;
 	private ServiceConnection mServiceConnection = new ServiceConnection() {
 		
 		   @Override
 		   public void onServiceConnected(ComponentName name, IBinder binder) {
+			   Log.d("D", "On Service Connected");
 		      LinnanmaaWeatherBinder weatherBinder = (LinnanmaaWeatherBinder)binder;
 		      mWeatherService = weatherBinder.getService();
 
@@ -51,8 +39,6 @@ public class LinnanmaaWeatherActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mTemperatureLabel = (TextView) findViewById(R.id.temperature_label);
-        mRefreshButton = (Button) findViewById(R.id.refresh_button);
-        new RefreshAsyncTask().execute();
         
 
         Intent intent = new Intent(this, LinnanmaaWeatherService.class);
@@ -60,63 +46,19 @@ public class LinnanmaaWeatherActivity extends Activity {
     }
     public void onRefreshButtonClick(View view) {
     	if (mWeatherService != null){
+    		Log.d("D", "Setting text");
     		mTemperatureLabel.setText(mWeatherService.getTemperature());
+    	}else{
+    		Log.d("D", "It's null!");
     	}
     }
     
-    private String getStringRegion(String string, String before, String after) {
-    	try {
-	    	int start = string.indexOf(before);
-	    	if (start == -1)
-	    		return null;
-	    	start += before.length();
-	    	int end = string.indexOf(after, start);
-	    	end -= start;
-	    	if (end == -1)
-	    		return null;
-	    	return string.substring(start, end);
-    	} catch (IndexOutOfBoundsException exception) {
-    		return null;
-    	}
-    }
-        
-    private class RefreshAsyncTask extends AsyncTask<Void, Void, String> {
-    	@Override
-		protected void onPreExecute() {
-    		mTemperatureLabel.setText(R.string.temperature_label);
-    		mRefreshButton.setEnabled(false);
-    	}
-    	@SuppressLint("NewApi")
-		@Override
-		protected String doInBackground(Void... params) {
-    		AndroidHttpClient httpClient = AndroidHttpClient.newInstance("Linnanmaa Weather (Android Example Application)");
-    		HttpUriRequest request = new HttpGet(WEATHER_URI);
-			try {
-				HttpResponse response = httpClient.execute(request);
-				if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK)
-					return "Server error.";
-				HttpEntity entity = response.getEntity();
-				String content = EntityUtils.toString(entity);
-				String temperature = getStringRegion(content, "<tempnow unit=\"C\">", "</tempnow>");
-				if (temperature == null)
-					return "Parse error.";
-				return temperature + " °C";
-			} catch (IOException e) {
-				return "Connection error.";
-			}
-		}
-		@Override
-		protected void onPostExecute(String result) {
-			mTemperatureLabel.setText(result);
-    		mRefreshButton.setEnabled(true);
-		}
-    	
-    }
     
 
     @Override
     protected void onStart() {
     	super.onStart();
+    	Log.d("D", "On Start");
     	Intent intent = new Intent(this, LinnanmaaWeatherService.class);
     	bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
     }
--- LinnanmaaWeather/src/fi/oulu/tol/linnanmaaweather/LinnanmaaWeatherService.java
+++ LinnanmaaWeather/src/fi/oulu/tol/linnanmaaweather/LinnanmaaWeatherService.java
@@ -1,24 +1,93 @@
 package fi.oulu.tol.linnanmaaweather;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.util.EntityUtils;
+
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
+import android.net.http.AndroidHttpClient;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
+@SuppressLint("NewApi")
 public class LinnanmaaWeatherService extends Service {
+	private final long seconds = 1000L;
+	
 	private final LinnanmaaWeatherBinder binder = new LinnanmaaWeatherBinder();
+	private static final String WEATHER_URI = "http://weather.willab.fi/weather.xml";
+	private String temperature = "Not available";
+	
+	TimerTask timerTask = new TimerTask() {
+		@Override
+		public void run(){
+			Log.d("D", "Running timer task");
+			AndroidHttpClient httpClient = AndroidHttpClient.newInstance("Linnanmaa Weather (Android Example Application)");
+    		HttpUriRequest request = new HttpGet(WEATHER_URI);
+			try {
+				HttpResponse response = httpClient.execute(request);
+				if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK){
+					temperature = "Server error";
+					return;
+				}
+				HttpEntity entity = response.getEntity();
+				String content = EntityUtils.toString(entity);
+				String temp = getStringRegion(content, "<tempnow unit=\"C\">", "</tempnow>");
+				if (temp == null){
+					temperature = "Parse error.";
+					return;
+				}
+				temperature =  temp + " °C";
+				return;
+			} catch (IOException e) {
+				temperature = "Connection error.";
+				return;
+			}
+		}
+	};
+	Timer timer = new Timer();
+	
+	
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		Log.d("D", "OnCreate");
+		timer.schedule(timerTask, 0L, 20 * this.seconds);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		
+		timer.cancel();
+	}
+
 	@Override
 	public IBinder onBind(Intent arg0) {
+		Log.d("D", "on bind");
 		return binder;
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.d("D", "on start command");
 		return START_STICKY;
 	}
 	
 	public String getTemperature() {
-		return "Unknown";
+		Log.d("D", "getTemperature");
+		return this.temperature;
 	}
 	
 
@@ -27,5 +96,21 @@ public class LinnanmaaWeatherService extends Service {
 			return LinnanmaaWeatherService.this;
 		}
 	}
+	
+    private String getStringRegion(String string, String before, String after) {
+    	try {
+	    	int start = string.indexOf(before);
+	    	if (start == -1)
+	    		return null;
+	    	start += before.length();
+	    	int end = string.indexOf(after, start);
+	    	end -= start;
+	    	if (end == -1)
+	    		return null;
+	    	return string.substring(start, end);
+    	} catch (IndexOutOfBoundsException exception) {
+    		return null;
+    	}
+    }
 
 }
